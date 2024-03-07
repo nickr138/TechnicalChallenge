d1d7315e4e27d769ed18af746d44a434a95af289
nickr138 <nickr138@aol.com>
2024-03-06 23:39:23 -0500

    Add in debugging printing
    Increment the timer counter to simulate a free-running timer

--- TechnicalChallenge/TechnicalChallenge.cpp
+++ TechnicalChallenge/TechnicalChallenge.cpp
@@ -2,12 +2,21 @@
 //
 
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 //****************************************
 // Constant definitions
 //****************************************
+
+// LED triggers after 65000 ticks
 const unsigned short TRIGGER_LED = 0xFDE8;
 
+// Number of ticks after 65000 that the LED logic can occur (30ms)
+const unsigned short EN_WINDOW = 0xD8;
+
+// Turn debugging printing on/off
+const bool PRINT_DEBUG = true;
 
 //****************************************
 // Timer Class
@@ -73,7 +82,7 @@ void Timer::IncrementTimerTick()
 int main()
 {
 	// Instantiate the timer
-	Timer timer;
+	Timer timer{};
 
 	// Both LEDs can be represented as booleans for simplicity. I'll start them in the "off" position
 	bool led1 = false;
@@ -82,14 +91,38 @@ int main()
 	// Intermediate variable for the input isn't necessary but does help debugging
 	short input;
 
+	// Enable flag so the program knows that a tick count of 65000 has been reached
+	bool en = false;
+
 	// Infinite loop to simulate the constant output from a sensor
 	while (1)
 	{
 		input = timer.GetTimerTick();
+
+		// Debugging printing, also so the console can see the tick that the system is at
+		if (PRINT_DEBUG)
+		{
+			std::printf("Tick Count: %hu\n", input);
+		}
+
 		if (input & TRIGGER_LED)
 		{
+			en = true;
 			// Normally, the function to blink the LED 5 times would be a callback that
-			// is added to the scheduler queue, but for simplicity it will just be 
+			// is added to the scheduler queue, but for simplicity the code will directly execute
+		}
+
+		// If needed, a 1ms delay can be added between each clock tick
+		//std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+		timer.IncrementTimerTick();
+
+		// If the enable flag is still set but the timer has gone past 216 ticks since it was enabled, disable it
+		input = timer.GetTimerTick();
+		if (en &&
+			(input & EN_WINDOW) == input)
+		{
+			en = false;
 		}
 	}
 }
